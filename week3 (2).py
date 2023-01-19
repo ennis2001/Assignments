e2d9ecf3427af24f55a773fb899f36d9b6c05780
Ennis <118567141+ennis2001@users.noreply.github.com>
2023-01-19 17:55:18 +0100

    Week 3

---
+++ week3 (2).py
@@ -0,0 +1,24 @@
+
+// Task: Example (demonstrating how to complete a task)
+// Define the variable "puppyName" and instanciate it with the name Fido
+let puppyName = "Fido";
+
+// Task 1
+// Fix the following code
+var puppyAge = 4
+
+// Task 2
+// Define a variable "breed" of type String  
+
+// Task 3
+// Set the value of breed to Labrador Retriever
+
+// Task 4
+// Fix the following code
+
+fun concatinatePuppyInfo(name,age,breed {
+  retun `${name}, ${age}, ${breed}  
+}
+
+// Task 5
+// Write a function convertDogToHumanAge that returns the human equivalent of dog years when we use the ratio 1:8 (i.e one dog year is eight human). Dog years should be given as a parameter to the function.
